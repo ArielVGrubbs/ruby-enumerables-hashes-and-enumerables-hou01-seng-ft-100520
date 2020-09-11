e72a227128a9aa1436ad9816d05b7fd36aec6dfc
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-11 00:01:26 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  passenger.each do |suite, name|
+  passengers.each do |suite, name|
     if suite == :suite_a && name.starts_with?("A")
       winner = name
     end
